3017aaaf3e89b15a27c3a58401911266f8fe6f8d
LaynezCoder <vlaynez2016@gmail.com>
2020-11-30 20:44:32 -0600

    A button is injected into the SaveImageControl class and the @FXML annotation was added.

--- src/com/laynezcoder/controller/SaveImageController.java
+++ src/com/laynezcoder/controller/SaveImageController.java
@@ -2,9 +2,14 @@ package com.laynezcoder.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 public class SaveImageController implements Initializable {
+    
+    @FXML
+    private Button btnOpenFileExplorer;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
