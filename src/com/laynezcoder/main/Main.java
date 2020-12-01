3cd77b10e6335a8d22b926b9d737950e7902cbb2
LaynezCoder <vlaynez2016@gmail.com>
2020-11-30 18:56:15 -0600

    Main class created

---
+++ src/com/laynezcoder/main/Main.java
@@ -0,0 +1,17 @@
+package com.laynezcoder.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+       
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
