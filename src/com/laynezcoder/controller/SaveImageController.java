231dbdd0416f94688c140e64186eaa1bc4fb2dce
LaynezCoder <vlaynez2016@gmail.com>
2020-11-30 20:46:22 -0600

    The openFileExplorer, getStage and handleOpenFileExplorer methods were created, the latter will be used in the .fxml file to control events.

--- src/com/laynezcoder/controller/SaveImageController.java
+++ src/com/laynezcoder/controller/SaveImageController.java
@@ -1,10 +1,18 @@
 package com.laynezcoder.controller;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javax.imageio.ImageIO;
 
 public class SaveImageController implements Initializable {
     
@@ -16,4 +24,27 @@ public class SaveImageController implements Initializable {
         
     }    
     
+    private void openFileExplorer() {
+        FileChooser fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter extFilterJPG = new FileChooser.ExtensionFilter("JPG files (*.jpg)", "*.JPG");
+        FileChooser.ExtensionFilter extFilterPNG = new FileChooser.ExtensionFilter("PNG files (*.png)", "*.PNG");
+        fileChooser.getExtensionFilters().addAll(extFilterJPG, extFilterPNG);
+        
+        File selectedFile = fileChooser.showOpenDialog(getStage());
+        if (selectedFile != null) {
+            try {
+                BufferedImage bufferedImage = ImageIO.read(selectedFile);
+                
+                System.out.println("Buffered Image: " + bufferedImage.getWidth() + " " + bufferedImage.getHeight());
+            } catch (IOException ex) {
+                Logger.getLogger(SaveImageController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else {
+            System.out.println("file is not valid!");
+        }
+    }
+
+    private Stage getStage() {
+        return (Stage) btnOpenFileExplorer.getScene().getWindow();
+    }  
 }
