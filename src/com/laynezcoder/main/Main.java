cd4baeff1b2edc085b5db56e82ecab9a7bd5621a
LaynezCoder <vlaynez2016@gmail.com>
2020-11-30 19:38:19 -0600

    The main class was modified.

--- src/com/laynezcoder/main/Main.java
+++ src/com/laynezcoder/main/Main.java
@@ -1,13 +1,28 @@
 package com.laynezcoder.main;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-       
+       try {
+            Parent root = FXMLLoader.load(getClass().getResource("/com/laynezcoder/view/SaveImage.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.initStyle(StageStyle.UNDECORATED);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     public static void main(String[] args) {
