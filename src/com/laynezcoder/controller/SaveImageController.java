81d7eef439c0c79f27d00924d64ad0abc025f887
LaynezCoder <vlaynez2016@gmail.com>
2020-11-30 19:03:01 -0600

    FXML file created with its respective controller class.

---
+++ src/com/laynezcoder/controller/SaveImageController.java
@@ -0,0 +1,14 @@
+package com.laynezcoder.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class SaveImageController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+    
+}
